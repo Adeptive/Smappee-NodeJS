51ed51e2e52268bf2dd7808441a9474dc5e9c925
michaelgeerts <geertsmichael@gmail.com>
2016-01-02 21:25:51 +0100

    Added more functions

--- test/smappee-api-test.js
+++ test/smappee-api-test.js
@@ -1,34 +1,46 @@
 var smappee = require('../my-smappee');
 var moment = require('moment');
 
-
-/*smappee.getServiceLocations(function(output) {
-    console.log(output);
-});
-
-smappee.getServiceLocationInfo("8039", function(output) {
-    console.log(output);
-});*/
-
-/*var from = moment().subtract(1, 'year').utc().valueOf();
+var from = moment().subtract(1, 'year').utc().valueOf();
 var to = moment().utc().valueOf();
 
-smappee.getConsumptions("8039", smappee.AGGREGATION_TYPES.MONTHLY, from, to, function(output) {
-    //console.log(output);
-});*/
+smappee.debug = false;
 
-/*smappee.getLatestConsumption("8039", function(output) {
+smappee.getServiceLocations(function(output) {
     console.log(output);
-});*/
+    console.log("");
 
+    var serviceLocationId = output.serviceLocations[0].serviceLocationId;
+    console.log("Working with first serviceLocationId: " + serviceLocationId);
+    console.log("");
 
-/*var from = moment().subtract(1, 'year').utc().valueOf();
- var to = moment().utc().valueOf();
+    smappee.getServiceLocationInfo(serviceLocationId, function(output) {
+        console.log('getServiceLocationInfo() -->');
+        console.log(output);
+        console.log("");
+    });
 
- smappee.getEvents("8039", "1", from, to, 10, function(output) {
- //console.log(output);
- });*/
+    smappee.getConsumptions(serviceLocationId, smappee.AGGREGATION_TYPES.MONTHLY, from, to, function(output) {
+        console.log('getConsumptions() -->');
+        console.log(output);
+        console.log("");
+    });
 
-smappee.turnActuatorOn("8039", "1", "300", function(output) {
-    console.log(output);
+    smappee.getLatestConsumption(serviceLocationId, function(output) {
+        console.log('getLatestConsumption() -->');
+        console.log(output);
+        console.log("");
+    });
+
+    smappee.getEvents(serviceLocationId, "1", from, to, 10, function(output) {
+        console.log('getEvents() -->');
+        console.log(output);
+        console.log("");
+    });
+
+    smappee.turnActuatorOn(serviceLocationId, 1, "300", function(output) {
+        console.log('turnActuatorOn() -->');
+        console.log(output);
+        console.log("");
+    });
 });
